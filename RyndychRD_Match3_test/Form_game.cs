6719e956e35dfde5b0d896164034a668b679fdd5
RyndychRD <r.roman1234@yandex.ru>
2020-12-04 00:25:33 +1000

    bad label

--- RyndychRD_Match3_test/Form_game.cs
+++ RyndychRD_Match3_test/Form_game.cs
@@ -14,21 +14,25 @@ namespace RyndychRD_Match3_test
     public partial class Form_game : Form
     {
         private readonly Form_main main;
+
+        bool is_debag_on = false;
+        int s = 60;
+
         public Form_game(Form_main Form_main)
         {
             InitializeComponent();
             main = Form_main;
 
             Draw_game_desk();
-            Figure_stek f = new Figure_stek();
-            f.Fill_table(this);
+            Figure_table f = new Figure_table(this);
+            f.Fill_table();
+            countdown_timer.Start();
 
 
         }
 
         private void Draw_game_desk()
         {
-            //TODO сменить bitmap на что-то без лишних затрат памяти
             Bitmap bmp = new Bitmap(t_game_desk.Width, t_game_desk.Height);
 
             Graphics g_game_desk = Graphics.FromImage(bmp);
@@ -56,53 +60,177 @@ namespace RyndychRD_Match3_test
             t_game_desk.BackgroundImage = bmp;
         }
 
-        private void b_Form_game_exit_Click(object sender, EventArgs e)
-        {
-            main.Show();
-            this.Close();
-        }
+       //TODO Вынести его за пределы этого класса
 
-        //TODO Вынести его за пределы этого класса
-
-        private class Figure_stek
+        private class Figure_table
         {
             Figure[,] figure_arr;
             Random rnd = new Random();
 
-            public Figure_stek()
-            {
+            Figure figure_first;
 
+            bool isClickedFirstFigure = false;
+            Form_game form_Game;
+
+            public Figure_table(Form_game form_Game_in)
+            {
+                form_Game = form_Game_in;
             }
 
-            public void Fill_table(Form_game form_Game)
+            public void Fill_table()
             {
-                figure_arr = new Figure[form_Game.t_game_desk.ColumnCount, form_Game.t_game_desk.RowCount];
-                for (int col = 0; col < form_Game.t_game_desk.ColumnCount; col++)
+                //TODO rewrite for random num
+                figure_arr = new Figure[8, 8];
+                for (int row = 0; row < 8; row++)
                 {
-                    for (int rows = 0; rows < form_Game.t_game_desk.RowCount; rows++)
+                    for (int col = 0; col < 8; col++)
                     {
-                        figure_arr[rows, col] = new Figure();
-                        form_Game.t_game_desk.Controls.Add(figure_arr[rows, col].getLabel, col, rows);
+                        //TODO придумать генерацию маски или создать несколько маск и их применять
+                        figure_arr[row, col] = new Figure(rnd.Next() % 5, row, col, this);
+
+                        figure_arr[row, col].label.Location = new Point(23 + col * 50, 48 + row * 57);
+                        form_Game.Controls.Add(figure_arr[row, col].label);
+                        figure_arr[row, col].label.BringToFront();
+
                     }
                 }
 
             }
-           
-            
-            private class Figure : Figure_stek
+
+            private void swap_figure(Figure figure_1, Figure figure_2)
             {
-                Label label = new Label()
+
+
+                Figure temp_fig1 = new Figure(figure_1);
+
+                int col1 = figure_1.col;
+                int row1 = figure_1.row;
+
+                int col2 = figure_2.col;
+                int row2 = figure_2.row;
+
+                Label l1 = new Label();
+                l1.BackColor = figure_1.label.BackColor;
+                Label l2 = new Label();
+                l2.BackColor = figure_2.label.BackColor;
+
+
+
+                int posx_figure_1 = figure_1.label.Left;
+                int posx_figure_2 = figure_2.label.Left;
+
+                int posy_figure_1 = figure_1.label.Top;
+                int posy_figure_2 = figure_2.label.Top;
+
+
+
+                figure_arr[row1, col1].label.BackColor = l2.BackColor;
+
+                figure_arr[row2, col2].label.BackColor = l1.BackColor; 
+
+
+
+
+                temp_fig1.col = col2;
+                temp_fig1.row = row2;
+                figure_arr[row2, col2] = temp_fig1;
+
+                //for(int i=0;i<10;i++)
+                //{
+                //    figure_arr[row1, col1].label.Top = figure_arr[row1, col1].label.Top + (posy_figure_1 - posy_figure_2)/10;
+                //    figure_arr[row2, col2].label.Top = figure_arr[row2, col2].label.Top + (posy_figure_1 - posy_figure_2) / 10;
+                //    figure_arr[row1, col1].label.Left += (posx_figure_1 - posx_figure_2) / 10;
+                //    figure_arr[row2, col2].label.Left += (posx_figure_1 - posx_figure_2) / 10;
+                //    Task.Delay(1000);
+                //}
+
+               // figure_arr[row1, col1].label.Location = new Point(posx_figure_1, posy_figure_1);
+               // figure_arr[row2, col2].label.Location = new Point(posx_figure_2, posy_figure_2);
+
+               
+
+                figure_arr[row1, col1].label.Text = "x=" + figure_arr[row1, col1].col + "; y=" + figure_arr[row1, col1].row;
+                figure_arr[row2, col2].label.Text = "x=" + figure_arr[row2, col2].col + "; y=" + figure_arr[row2, col2].row;
+
+
+            }
+
+            private bool is_near(Figure figure_1, Figure figure_2)
+            {
+                if ((Math.Abs(figure_1.col - figure_2.col) == 1) ^ (Math.Abs(figure_1.row - figure_2.row) == 1))
                 {
-                    Margin = new Padding(4, 4, 4, 4),
-                    Dock = DockStyle.Fill,
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            private void Label_Click(object sender, EventArgs e, int row, int col, Figure_table figure_table)
+            {
+                if (isClickedFirstFigure)
+                {
+                    Figure temp= figure_arr[row, col];
+
+                    figure_arr[row, col].label.Text = "2 click";
+
+                    //if (!(form_Game.is_debag_on))
+                    //{
+                    //    if (is_near(figure_first, figure_arr[row, col]))
+                    //    {
+                    //        swap_figure(figure_first, figure_arr[row, col]);
+                    //    }
+                    //}
+                    //else
+                    //{
+                        swap_figure(figure_first, figure_arr[row, col]);
+                   // }
+                    isClickedFirstFigure = false;
+                    figure_first = null;
+                }
+                else
+                {
+                    figure_first = figure_arr[row,col];
+                    figure_first.label.Text = "1 click";
+                    isClickedFirstFigure = true;
+                    //TODO light it up
+
+                }
+
+            }
+
+
+            public class Figure
+            {
+                public Label label = new Label()
+                {
+                    Size = new Size(45, 52),
 
                 };
 
-                int type;
-                public Figure()
-                //TODO заменить на картинки
+                public int type;
+
+
+                public int row;
+
+                public int col;
+
+
+
+                public Figure(Figure figure_to_copy)
                 {
-                    switch (rnd.Next() % 5)
+                    type = figure_to_copy.type;
+                    row = figure_to_copy.row;
+                    col = figure_to_copy.col;
+                    label = figure_to_copy.label;
+                }
+
+                public Figure(int typeIn, int row_in, int col_in, Figure_table figure_table)
+                {
+                    label.Click += (sender, e) => figure_table.Label_Click(sender, e, this.row, this.col, figure_table);
+
+                    switch (typeIn)
                     {
                         case 0:
                             label.BackColor = Color.Red;
@@ -125,20 +253,49 @@ namespace RyndychRD_Match3_test
                             type = 4;
                             break;
                     }
-
+                    row = row_in;
+                    col = col_in;
 
                 }
-                public Label getLabel
-                {
-                    get => label;
-                }
+
+
+
             }
-            
+
 
 
         }
 
+        private void Form_game_debug_Click(object sender, EventArgs e)
+        {
+            is_debag_on = !(is_debag_on);
+            if (is_debag_on)
+            {
+                Form_game_debug.Text = "Debug on";
+                countdown_timer.Stop();
+            }
+            else
+            {
+                Form_game_debug.Text = "Debug off";
+                countdown_timer.Start();
+            }
 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            s = s - 1;
+            this.countdown_label.Text = s.ToString();
+            if (s == 0)
+            {
+                countdown_timer.Stop();
+                MessageBox.Show("Game over");
+                main.Show();
+                this.Close();
+
+            }
+
+        }
     }
 
 
